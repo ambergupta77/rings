648919d4567b343dbbe7dd90c9bef0b00e936f1a
ambergupta77 <ambergupta77@gmail.com>
2020-07-24 14:32:52 +0100

    updated by java lambda style

--- src/main/java/com/uno/agrona/AgronaAgent.java
+++ src/main/java/com/uno/agrona/AgronaAgent.java
@@ -2,20 +2,28 @@ package com.uno.agrona;
 /**
  * Unlike LMAX here we can only have byte buffers stored.
  */
+//TODO change the read to lambda style
+
 
 import org.agrona.MutableDirectBuffer;
 import org.agrona.concurrent.Agent;
-import org.agrona.concurrent.UnsafeBuffer;
-import org.agrona.concurrent.ringbuffer.OneToOneRingBuffer;
+import org.agrona.concurrent.MessageHandler;
 import org.agrona.concurrent.ringbuffer.RingBuffer;
-
-import java.nio.ByteBuffer;
-
-import static org.agrona.concurrent.ringbuffer.RingBufferDescriptor.TRAILER_LENGTH;
 
 public class AgronaAgent implements Agent {
     public static final int  MY_EVENT_MSG_ID = 99;
     private RingBuffer ringBuffer;
+
+    private MessageHandler messageHandler = this::read;
+
+    private void read(int msgTypeId, MutableDirectBuffer mutableDirectBuffer, int index, int length) {
+        if (msgTypeId == MY_EVENT_MSG_ID) {
+            int anInt = mutableDirectBuffer.getInt(index);
+            long aLong = mutableDirectBuffer.getLong(index + 4);
+            String stringAscii = mutableDirectBuffer.getStringAscii(index + 4 + 8);
+            System.out.println("Received messages with Int: " + anInt + " Long:" + aLong + " String: " + stringAscii);
+        }
+    }
 
     public AgronaAgent(RingBuffer ringBuffer) {
         this.ringBuffer = ringBuffer;
@@ -24,14 +32,18 @@ public class AgronaAgent implements Agent {
 
     @Override
     public int doWork() throws Exception {
-        return ringBuffer.read((int msgTypeId ,  MutableDirectBuffer mutableDirectBuffer, int index, int length) -> {
+        //Style 1
+        return ringBuffer.read(messageHandler);
+        //Style2
+        /*return ringBuffer.read((int msgTypeId ,  MutableDirectBuffer mutableDirectBuffer, int index, int length) -> {
             if (msgTypeId == MY_EVENT_MSG_ID) {
-                    int anInt = mutableDirectBuffer.getInt(index);
-                    long aLong = mutableDirectBuffer.getLong(index + 4);
-                    String stringAscii = mutableDirectBuffer.getStringAscii(index + 4 + 8);
-                    System.out.println("Received messages with Int: " + anInt + " Long:" + aLong + " String: " + stringAscii);
+                int anInt = mutableDirectBuffer.getInt(index);
+                long aLong = mutableDirectBuffer.getLong(index + 4);
+                String stringAscii = mutableDirectBuffer.getStringAscii(index + 4 + 8);
+                System.out.println("Received messages with Int: " + anInt + " Long:" + aLong + " String: " + stringAscii);
             }
-        });
+        });*/
+        //Style 3
         /*return  ringBuffer.read(new MessageHandler() {
             @Override
             public void onMessage(int msgTypeId ,  MutableDirectBuffer mutableDirectBuffer, int index, int length) {
